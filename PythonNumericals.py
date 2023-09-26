b626250cfe637cb229c39a289b477b33d1fe9504
BeginnerUser-vss <145959593+BeginnerUser-vss@users.noreply.github.com>
2023-09-26 17:02:40 +0530

    Python Numericals
    For Practice

---
+++ PythonNumericals.py
@@ -0,0 +1,43 @@
+# Python program to convert decimal numbers to octal number
+
+def decToOct(n):
+    print(oct(n))
+ 
+n = 33
+decToOct(n)
+
+# Python program to reverse an integer
+
+def reverseInteger(integer):
+    result=0
+
+    while integer!=0:
+        remainder = integer % 10
+        result = result * 10 + remainder
+        integer //= 10
+
+    return result
+
+print("The reverse of 43 is ",reverseInteger(43))
+
+# Python Program to print Fibonacci series with Recursive method 
+
+def recursiveFibo(n):
+    if n<=1:
+        return n
+    else:
+        return (recursiveFibo(n-1)+recursiveFibo(n-2))
+
+fiboNum = 9
+print("The fibonacci series of number",fiboNum)
+print(recursiveFibo(fiboNum))
+
+for i in fiboNum:
+    print(recursiveFibo(fiboNum))
+
+
+
+
+
+
+    
---
+++ README.md
@@ -0,0 +1,7 @@
+# Python Numerical Programs Training
+
+Write a program in Python to -
+
+- Convert decimal numbers to octal numbers.
+- Reverse an integer.
+- Print the Fibonacci series using the recursive method.
